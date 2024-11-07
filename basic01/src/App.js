31305000655b27bb55bf4f29fa346020d7aa4e68
Akash Manwar <akashmanwar@Akashs-MacBook-Air.local>
2024-11-07 13:44:17 +0530

    Learnings from video 2

---
+++ basic01/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Chai aur Code | Akash Manwar</h1>
+  );
+}
+
+export default App;
---
+++ viteBasic01/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Chai aur Code with vite | Akash Manwar</h1>
+    </>
+  )
+}
+
+export default App
